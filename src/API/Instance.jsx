c9166ca9edaedb6bebbec54e4fed4ef7fb313590
Omar AL AKOUM <omar.alakoum@outlook.com>
2025-03-28 13:55:28 +0200

    y

--- src/API/Instance.jsx
+++ src/API/Instance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_URL = 'http://localhost:4000/api';
-const API_URL='https://node-firstproject-3.onrender.com'
+const API_URL='https://node-firstproject-3.onrender.com/api'
 const axiosInstance = axios.create({
   baseURL: API_URL,
   timeout: 10000,
